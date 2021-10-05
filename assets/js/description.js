dbb4c4de2c1a040d8820fc1e08c3bb45f22727e4
Brent Graves <graves.brent@gmail.com>
2021-10-05 18:27:29 -0400

    started a second JS file to add a description for each hero in the load screen.

---
+++ assets/js/description.js
@@ -0,0 +1,22 @@
+var heroPowers = [
+    {    
+        character: "Spider-Man",
+        description: "Bitten by a radioactive spider, Peter Parker has Superhuman strength, speed, reflexes, agility, coordination and balance that allows him to swing into action!"
+    },
+    {
+        character: "Thor",
+        description: "With the help of Mjolnir, the god of thunder possesses flight, thunder, electrical manipulation along with Superhuman strength. The son of Odin is rarely defeated in a fight!"
+    },
+    {
+        character: "Gamora",
+        description: "The adopted daughter of Thanos is an expert assassin. Gamora possesses Superhuman strength, speed, agility and durability."
+    },
+
+    {
+        character: "Iron Man",
+        description: "Genius Tony Stark uses a arc reactor to power his suit for supersonic flight, durability, reflexes, energy repulsor and missile projections. Iron man is ready for any fight."
+    },
+    {
+        character: ""
+    }
+]
