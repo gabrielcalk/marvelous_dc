4168176f510be1f0130f10f0906bb872a8a1366c
Brent Graves <graves.brent@gmail.com>
2021-10-04 22:48:02 -0400

    added an array for superheroapi that needs to be connected to powerstats.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-characterArray = ["Spider-Man",  "Thor",  "Iron Man", "Hulk", "Captain America", "Hawkeye", "Captain Marvel", "Doctor Strange", "Black Panther", "Black Widow", "Winter Soldier", "Vision", "Ant-Man", "Scarlet Witch",  "Wasp", "Falcon", "Loki", "Thanos", "Ultron", "Groot", "Drax", "Peter Quill", "War Machine", "Nebula", "Gamora"]
+comVineKey = "e66e9aa178806d867789420a8e86e23da2db2d58"
+comicvineURL = "https://comicvine.gamespot.com/api/"
+
+marvelpublicKey = "1c1a706c2a70ef2ebc3798fe2bf6f2a0"
+marvelprivateKey = "178c686836b4256972b2404a177d117753022fe4"
+marveldevURL = "https://developer.marvel.com/docs"
+
+superheroToken = "1408209922908916"
+//access token
+superheroURL = "https://superheroapi.com/api/1408209922908916"
+
+//array for marvel 
+characterArray = ["Spider-Man",  "Thor",  "Iron Man", "Hulk", "Captain America", "Hawkeye", "Captain Marvel", "Doctor Strange", "Black Panther", "Black Widow", "Winter Soldier", "Vision", "Ant-Man", "Scarlet Witch",  "Wasp", "Falcon", "Loki", "Thanos", "Ultron", "Groot", "Drax", "star-lord", "War Machine", "Nebula", "Gamora"]
+
+//array for superhero api "powerstats" is keyword
+characterArraysuper = ["spider-man", "thor", "ironman", "hulk", "captain america", "hawkeye", "captain marvel", "doctor strange", "black panther", "black widow", "winter soldier", "vision", "ant-man", "scarlet witch", "wasp", "falcon", "loki", "thanos", "ultron", "groot", "drax", "star-lord", "war machine", "nebula", "gamora"]
