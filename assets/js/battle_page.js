588a2d3157afa945cec174a3947444046dc39ef1
Gabriel Cavalcante <gabrielcalk@outlook.com>
2021-10-05 12:21:55 -0400

    Adding the process of the battle page on js

---
+++ assets/js/battle_page.js
@@ -0,0 +1,30 @@
+// set the one variable to 5 (the user can only choose 5 heroes) - Starting with 1
+
+// Adding the heroes selected on the page that has the select class
+
+
+// Add the heroes name on the div - option 1
+console.log(characterArray.length)
+function add_heroes() {
+    for (i = 0; i < characterArray.length; i++){
+
+    }
+    
+}
+
+// Add the heroes description on the div
+
+// Add the heroes picture on the div (probably using html)
+
+//Set attribute on the heroes DIV and add a select class(click)
+
+
+
+
+
+//Set Attribute on the button that start the battle (starting the battle function)
+
+//battle function: go to the next page
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,170 +19,111 @@ characterArraysuper = ["spider-man", "thor", "ironman", "hulk", "captain america
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=0')
 .then(function (response){if (!response.ok) {alert('API error');}
   return response.json();
-}).then(function (data1){console.log(data1)})
+}).then(function (data1){
+    console.log(data1)})
 
-// Resquesting the first 200characters marvel
+// Resquesting the 100 - 200 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=100')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data2){
+    console.log(data2)})
 
+// Resquesting the first 200 - 300 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=200')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data3){
+    console.log(data3)})
 
+// Resquesting the first 300 - 400 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=300')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data4){
+    console.log(data4)})
 
+// Resquesting the first 400 - 500 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=400')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data5){
+    console.log(data5)})
 
+// Resquesting the first 500 - 600 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=500')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data6){
+    console.log(data6)})
 
+// Resquesting the first 600 - 700 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=600')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data7){
+    console.log(data7)})
 
+// Resquesting the first 700 - 800 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=700')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data8){
+    console.log(data8)})
 
+// Resquesting the first 800 - 900 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=800')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data9){
+    console.log(data9)})
 
+// Resquesting the first 900 - 1000 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=900')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data10){
+    console.log(data10)})
 
+// Resquesting the first 1000 - 1100 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=1000')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data11){
+    console.log(data11)})
 
+// Resquesting the first 1100 - 1200 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=1100')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data12){
+    console.log(data12)})
 
+// Resquesting the first 1200 - 1300 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=1200')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data13){
+    console.log(data13)})
 
+// Resquesting the first 1300 - 1400 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=1300')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data14){
+    console.log(data14)})
 
+// Resquesting the 1400 - 1500 characters marvel
 fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=1400')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+.then(function (response){if (!response.ok) {alert('API error');}
+  return response.json();
+}).then(function (data15){
+    console.log(data15)})
 
-fetch(marveldevURL+marvelpublicKey+marvelhash+'&limit=100&offset=1500')
-.then(function (response){
-    if (!response.ok) {
-    alert('API error');
-    }
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-})
+//
+//
+//
+//
+//
+//
+//
