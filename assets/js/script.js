7086ead59ff420cb716a6c38286c173d2e4d9493
Brent Graves <graves.brent@gmail.com>
2021-10-04 22:52:46 -0400

    changed spelling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ superheroToken = "1408209922908916"
 superheroURL = "https://superheroapi.com/api/1408209922908916"
 
 //array for marvel 
-characterArray = ["Spider-Man",  "Thor",  "Iron Man", "Hulk", "Captain America", "Hawkeye", "Captain Marvel", "Doctor Strange", "Black Panther", "Black Widow", "Winter Soldier", "Vision", "Ant-Man", "Scarlet Witch",  "Wasp", "Falcon", "Loki", "Thanos", "Ultron", "Groot", "Drax", "star-lord", "War Machine", "Nebula", "Gamora"]
+characterArray = ["Spider-Man",  "Thor",  "Iron Man", "Hulk", "Captain America", "Hawkeye", "Captain Marvel", "Doctor Strange", "Black Panther", "Black Widow", "Winter Soldier", "Vision", "Ant-Man", "Scarlet Witch",  "Wasp", "Falcon", "Loki", "Thanos", "Ultron", "Groot", "Drax", "star lord", "War Machine", "Nebula", "Gamora"]
 
 //array for superhero api "powerstats" is keyword
 characterArraysuper = ["spider-man", "thor", "ironman", "hulk", "captain america", "hawkeye", "captain marvel", "doctor strange", "black panther", "black widow", "winter soldier", "vision", "ant-man", "scarlet witch", "wasp", "falcon", "loki", "thanos", "ultron", "groot", "drax", "star-lord", "war machine", "nebula", "gamora"]
