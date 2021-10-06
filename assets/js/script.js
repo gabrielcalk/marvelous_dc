4e59ac86d58e59d9d7dc486e87724349cf99c4cc
Gabriel <gabrielcalk@outlook.com>
2021-10-06 10:53:59 -0400

    Delete some api

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+comVineKey = "e66e9aa178806d867789420a8e86e23da2db2d58"
+comicvineURL = "https://comicvine.gamespot.com/api/"
+
+marvelpublicKey = "10ac0c8d75dd78300bd5c3366134bf16"
+marvelhash = "&hash=2965ad94a5b4a122eb9666674c776754"
+marveldevURL = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey="
+
+superheroToken = "1408209922908916"
+//access token
+superheroURL = "https://superheroapi.com/api/1408209922908916"
+
+//array for marvel 
+characterArray = ["Spider-Man",  "Thor",  "Iron Man", "Hulk", "Captain America", "Hawkeye", "Captain Marvel", "Doctor Strange", "Black Panther", "Black Widow", "Winter Soldier", "Vision", "Ant-Man", "Scarlet Witch",  "Wasp", "Falcon", "Loki", "Thanos", "Ultron", "Groot", "Drax", "star lord", "War Machine", "Nebula", "Gamora"]
+
+
+
+//following are notes to the id of the character and path to thumbnail
+
+//ant-man (scott lang) id: 1010801 thumbnail: {path: 'http://i.annihil.us/u/prod/marvel/i/mg/e/20/52696868356a0', extension: 'jpg'} no description
+
+//Black Panther id: 1009187 {path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/60/5261a80a67e7d', extension: 'jpg'} no description
+
+//Black Widow id: 1009189 {path: 'http://i.annihil.us/u/prod/marvel/i/mg/f/30/50fecad1f395b', extension: 'jpg'} no description
+
+//Captain America id: 1017105 {path: 'http://i.annihil.us/u/prod/marvel/i/mg/3/10/52321928eaa72', extension: 'jpg'} has description
+
+//Captain Marvel id: 1010338  {path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/80/5269608c1be7a', extension: 'jpg'} no description
+
+//Hawkeye id: 1017108 {path: 'http://i.annihil.us/u/prod/marvel/i/mg/4/03/5232198a81c17', extension: 'jpg'} has description
+
+//Hulk id: 1009351 {path: 'http://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0', extension: 'jpg'} has description
+
+
+
+//array for superhero api "powerstats" is keyword
+characterArraysuper = ["spider-man", "thor", "ironman", "hulk", "captain america", "hawkeye", "captain marvel", "doctor strange", "black panther", "black widow", "winter soldier", "vision", "ant-man", "scarlet witch", "wasp", "falcon", "loki", "thanos", "ultron", "groot", "drax", "star-lord", "war machine", "nebula", "gamora"]
+
+
